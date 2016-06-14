ec51ec379b1451776dd7a1839da1da5d1f0c929a
Fab <INTI-0283@INTI0283>
2016-06-14 15:23:43 +0200

    MAJ contrat

--- immobelia/src/main/java/fr/adaming/model/Contrat.java
+++ immobelia/src/main/java/fr/adaming/model/Contrat.java
@@ -32,6 +32,97 @@ public class Contrat {
 
 	@Column(name = "validationContrat")
 	private boolean validationContrat;
+	
+	/**
+	 * ctor vide
+	 */
+	public Contrat() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * ctor chargé avec id
+	 * @param idContrat
+	 * @param conseillerId
+	 * @param clientId
+	 * @param bienImmobilierId
+	 * @param validationContrat
+	 */
+	public Contrat(int idContrat, ConseillerImmobilier conseillerId,
+			Client clientId, BienImmobilier bienImmobilierId,
+			boolean validationContrat) {
+		super();
+		this.idContrat = idContrat;
+		this.conseillerId = conseillerId;
+		this.clientId = clientId;
+		this.bienImmobilierId = bienImmobilierId;
+		this.validationContrat = validationContrat;
+	}
+
+	/**
+	 * ctor chargé sans id
+	 * @param conseillerId
+	 * @param clientId
+	 * @param bienImmobilierId
+	 * @param validationContrat
+	 */
+	public Contrat(ConseillerImmobilier conseillerId, Client clientId,
+			BienImmobilier bienImmobilierId, boolean validationContrat) {
+		super();
+		this.conseillerId = conseillerId;
+		this.clientId = clientId;
+		this.bienImmobilierId = bienImmobilierId;
+		this.validationContrat = validationContrat;
+	}
+
+	/**************************
+	 * GETTERS & SETTERS
+	 *
+	 *************************/	
+	
+	public int getIdContrat() {
+		return idContrat;
+	}
+
+	public void setIdContrat(int idContrat) {
+		this.idContrat = idContrat;
+	}
+
+	public ConseillerImmobilier getConseillerId() {
+		return conseillerId;
+	}
+
+	public void setConseillerId(ConseillerImmobilier conseillerId) {
+		this.conseillerId = conseillerId;
+	}
+
+	public Client getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Client clientId) {
+		this.clientId = clientId;
+	}
+
+	public BienImmobilier getBienImmobilierId() {
+		return bienImmobilierId;
+	}
+
+	public void setBienImmobilierId(BienImmobilier bienImmobilierId) {
+		this.bienImmobilierId = bienImmobilierId;
+	}
+
+	public boolean isValidationContrat() {
+		return validationContrat;
+	}
+
+	public void setValidationContrat(boolean validationContrat) {
+		this.validationContrat = validationContrat;
+	}
+	
+	
+	
+	
 
 	
 
