67c902780b071e6edcb5ce0bb9a2757de764ed8b
Thomas <inti@INTI0290>
2016-06-14 14:18:37 +0200

    Classe (sans onetoMany)

---
+++ immobelia/src/main/java/fr/adaming/model/ConseillerImmobilier.java
@@ -0,0 +1,101 @@
+package fr.adaming.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="conseillerImmobilier")
+@Table(name="conseillersImmobilier")
+public class ConseillerImmobilier extends Personne {
+
+	/****************************************************************************/
+	/****************************** DECLARATIONS ********************************/
+	/****************************************************************************/
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_conseiller")
+	private int id_conseiller;
+	
+	@Column(name="password")
+	private String motDePasse;
+
+	/****************************************************************************/
+	/****************************** CONSTRUCTEURS *******************************/
+	/****************************************************************************/
+	
+	/**
+	 * Constructeur Chargé
+	 * @param nom
+	 * @param prenom
+	 * @param id_personne
+	 * @param telephone
+	 * @param adresse
+	 * @param id_conseiller
+	 * @param motDePasse
+	 */
+	public ConseillerImmobilier(String nom, String prenom, int id_personne,
+			String telephone, Adresse adresse, int id_conseiller,
+			String motDePasse) {
+		super(nom, prenom, id_personne, telephone, adresse);
+		this.id_conseiller = id_conseiller;
+		this.motDePasse = motDePasse;
+	}
+
+	/**
+	 * Constructeur sans Id
+	 * @param nom
+	 * @param prenom
+	 * @param id_personne
+	 * @param telephone
+	 * @param adresse
+	 * @param motDePasse
+	 */
+	public ConseillerImmobilier(String nom, String prenom, int id_personne,
+			String telephone, Adresse adresse, String motDePasse) {
+		super(nom, prenom, id_personne, telephone, adresse);
+		this.motDePasse = motDePasse;
+	}
+
+	/**
+	 * Constructeur vide
+	 */
+	public ConseillerImmobilier() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/****************************************************************************/
+	/********************************* METHODES *********************************/
+	/****************************************************************************/
+	
+	@Override
+	public String toString() {
+		return "ConseillerImmobilier [id_conseiller=" + id_conseiller
+				+ ", motDePasse=" + motDePasse + "]";
+	} // Fin methode toString
+
+	/****************************************************************************/
+	/****************************** GETTERS_SETTERS *****************************/
+	/****************************************************************************/
+	
+	public int getId_conseiller() {
+		return id_conseiller;
+	}
+
+	public void setId_conseiller(int id_conseiller) {
+		this.id_conseiller = id_conseiller;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+		
+} // Fin classe ConseillerImmobilier
---
+++ immobelia/src/main/java/fr/adaming/model/Personne.java
@@ -0,0 +1,136 @@
+package fr.adaming.model;
+
+// Imports
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="personne")
+@Table(name="personnes")
+public abstract class Personne {
+
+	/****************************************************************************/
+	/****************************** DECLARATIONS ********************************/
+	/****************************************************************************/
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_personne")
+	private int id_personne;
+	
+	@Column(name="nom")
+	private String nom;
+	
+	@Column(name="prenom")
+	private String prenom;
+	
+	@Column(name="telephone")
+	private String telephone;
+	
+	@Column(name="adresse")
+	private Adresse adresse;
+
+	/****************************************************************************/
+	/****************************** CONSTRUCTEURS *******************************/
+	/****************************************************************************/
+
+	/**
+	 * Constructeur chargé
+	 * 
+	 * @param nom
+	 * @param prenom
+	 * @param id_personne
+	 * @param telephone
+	 * @param adresse
+	 */
+	public Personne(String nom, String prenom, int id_personne,
+			String telephone, Adresse adresse) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.id_personne = id_personne;
+		this.telephone = telephone;
+		this.adresse = adresse;
+	} // Fin constructeur chargé
+
+	/**
+	 * Constructeur sans Id
+	 * 
+	 * @param nom
+	 * @param prenom
+	 * @param telephone
+	 * @param adresse
+	 */
+	public Personne(String nom, String prenom, String telephone, Adresse adresse) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.telephone = telephone;
+		this.adresse = adresse;
+	} // Fin constructeur sans Id
+
+	/**
+	 * Constructeur vide
+	 */
+	public Personne() {
+		super();
+	} // Fin constructeur vide
+
+	/****************************************************************************/
+	/********************************* METHODES *********************************/
+	/****************************************************************************/
+
+	@Override
+	public String toString() {
+		return "Personne [nom=" + nom + ", prenom=" + prenom + ", id_personne="
+				+ id_personne + ", telephone=" + telephone + "]";
+	} // Fin methode toString
+
+	/****************************************************************************/
+	/****************************** GETTERS_SETTERS *****************************/
+	/****************************************************************************/
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getId_personne() {
+		return id_personne;
+	}
+
+	public void setId_personne(int id_personne) {
+		this.id_personne = id_personne;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+} // Fin classe Personne
