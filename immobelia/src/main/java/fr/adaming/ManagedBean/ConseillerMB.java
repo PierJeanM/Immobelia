24da116b1615c84802bc4af8dae4cb8b132c1a2e
Fab <fab.vignon@gmail.com>
2016-06-16 10:48:01 +0200

    MAJ MB+homepage

--- immobelia/src/main/java/fr/adaming/ManagedBean/ConseillerMB.java
+++ immobelia/src/main/java/fr/adaming/ManagedBean/ConseillerMB.java
@@ -38,7 +38,7 @@ public class ConseillerMB {
 					&& cons.getMotDePasse().equals(
 							conseillerImmobilier.getMotDePasse())) {
 				setConseillerImmobilier(cons);
-				return "/pages/homepage.xhtml?faces-redirect=true";
+				return "/WEB-INF/pages/homepage.xhtml?faces-redirect=true";
 			}
 		}
 		FacesContext.getCurrentInstance().addMessage(null,
